60eb8a78a696625cec8c6d11cbbec64c8b21e64a
Miezhiko <Miezhiko@gmail.com>
2022-12-06 16:11:06 +0400

    Fix terminal title
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- .zsh/conf.sh
+++ .zsh/conf.sh
@@ -23,23 +23,32 @@ autoload -U zargs
   SEGMENT_SEPARATOR='>'
 }
 
-termtitle() {
-  case "$TERM" in
-    rxvt*|xterm|gnome-terminal|tmux)
-      case "$1" in
-        precmd)
-          print -Pn "\e]0;%n@%m: %~\a"
-        ;;
-        preexec)
-          zsh_cmd_title="$2"
-          zsh_cmd_title="${zsh_cmd_title//\\/\\\\}"
-          zsh_cmd_title="${zsh_cmd_title//\$/\\\\\$}"
-          zsh_cmd_title="${zsh_cmd_title//\%/<percent>}"
-          print -Pn "\e]0;${zsh_cmd_title} [%n@%m: %~]\a"
-        ;;
+[[ ${TERM} != dumb ]] && () {
+  local termtitle_format
+  local zhooks zhook
+  zstyle -a ':zim:termtitle' hooks 'zhooks' || zhooks=(precmd)
+  setopt prompt{percent,subst}
+  autoload -Uz add-zsh-hook
+  for zhook in ${zhooks}; do
+    if [[ ${TERM_PROGRAM} == Apple_Terminal ]]; then
+      termtitle_update_${zhook}() {
+        print -n "\E]7;${PWD}\a"
+      }
+    else
+      zstyle -s ":zim:termtitle:${zhook}" format 'termtitle_format' || \
+          zstyle -s ':zim:termtitle' format 'termtitle_format' || \
+          termtitle_format='%n@%m: %~'
+      case ${TERM} in
+        screen)
+          builtin eval "termtitle_update_${zhook}() { print -Pn '\Ek'${(qq)termtitle_format}'\E\\' }"
+          ;;
+        *)
+          builtin eval "termtitle_update_${zhook}() { print -Pn '\E]0;'${(qq)termtitle_format}'\a' }"
+          ;;
       esac
-    ;;
-  esac
+    fi
+    add-zsh-hook ${zhook} termtitle_update_${zhook}
+  done
 }
 
 # Completion.
