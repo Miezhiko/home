941b4948d09bb0a69317ccf7a0e70ebf960b8e7a
Miezhiko <Miezhiko@gmail.com>
2025-05-13 13:29:58 +0400

    add timers
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- .zsh/func.sh
+++ .zsh/func.sh
@@ -52,3 +52,21 @@ kill-port() {
     echo "'$1' is not a valid port"
   fi
 }
+
+countdown() {
+    start="$(( $(date '+%s') + $1))"
+    while [ $start -ge $(date +%s) ]; do
+        time="$(( $start - $(date +%s) ))"
+        printf '%s\r' "$(date -u -d "@$time" +%H:%M:%S)"
+        sleep 0.1
+    done
+}
+
+stopwatch() {
+    start=$(date +%s)
+    while true; do
+        time="$(( $(date +%s) - $start))"
+        printf '%s\r' "$(date -u -d "@$time" +%H:%M:%S)"
+        sleep 0.1
+    done
+}
